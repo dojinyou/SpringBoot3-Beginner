2f81da54d501d2f249aae3561355eea703c14209
dojinyou <djyou128@gmail.com>
2023-02-19 22:33:30 +0900

    create springboot3 project

---
+++ example/src/main/kotlin/com/dojinyou/books/springboot3beginner/ExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.dojinyou.books.springboot3beginner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExampleApplication>(*args)
+}
