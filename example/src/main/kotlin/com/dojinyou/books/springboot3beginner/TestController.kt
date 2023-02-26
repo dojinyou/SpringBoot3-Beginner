cefb06f41c8c7ddd1693f404dda738bacf08895f
dojinyou <djyou128@gmail.com>
2023-02-26 23:28:39 +0900

    Controller 생성

---
+++ example/src/main/kotlin/com/dojinyou/books/springboot3beginner/TestController.kt
@@ -0,0 +1,14 @@
+package com.dojinyou.books.springboot3beginner
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "Hello, world!"
+    }
+
+}
